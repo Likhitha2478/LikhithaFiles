4ec936343c3a91c2c902adea32e7ab74b2a94e06
likhi <likhi@192.168.1.37>
2024-10-25 04:07:39 +0530

    commit 3

---
+++ SVKP_college/src/basicsofjava/ArrayDemo.java
@@ -0,0 +1,12 @@
+package basicsofjava;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		int num[]= {1,2,3};
+		System.out.println(num[0]);
+		System.out.println(num[2]);
+
+	}
+
+}
