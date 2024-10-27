5b7554eaeeab344250e7b34fbd7984e67cebdc6b
likhi <likhi@192.168.1.41>
2024-10-28 02:26:13 +0530

    commit 4

---
+++ SVKP_college/src/basicsofjava/WhileloopDemo.java
@@ -0,0 +1,15 @@
+package basicsofjava;
+
+public class WhileloopDemo {
+
+	public static void main(String[] args) {
+		int i=1;
+		while(i<10)
+		{
+			System.out.println("Tomorrow is holiday "+i);
+			i++;
+		}
+
+	}
+
+}
