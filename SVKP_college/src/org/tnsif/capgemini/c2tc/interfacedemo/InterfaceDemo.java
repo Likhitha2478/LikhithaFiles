2db775d61dbeea28efaa4ab6299c52462abe2651
likhi <likhi@Srinu>
2025-03-29 21:40:19 +0530

    commit

--- SVKP_college/src/org/tnsif/capgemini/c2tc/interfacedemo/InterfaceDemo.java
+++ SVKP_college/src/org/tnsif/capgemini/c2tc/interfacedemo/InterfaceDemo.java
@@ -1,20 +1,30 @@
 package org.tnsif.capgemini.c2tc.interfacedemo;
 
 public class InterfaceDemo {
+public class Vehicle{
+	public class Car extends Vehicle {
+		
+
+public class Bike extends Vehicle {
 
 	public static void main(String[] args) {
 		//you can't instantiate the interface itself
-				Vehicle car=new Car("Toyota corolla");
-				Vehicle bike=new Bike("Mountain Bike");
+				Vehicle car=new Car();
+				Vehicle bike=new Bike();
 				
+				public void start() {
+				}			
 				car.start();
 				car.stop();
 				
 				bike.start();
 				bike.stop();
-				
+			
 				System.out.println("Maximun speed for vehicle" +Vehicle.Max_Speed);
-			}
+	   
+	  }
 	}
-
+ }
+}
+}
 
