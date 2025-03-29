be208d78f2599fc1fe5ce32086fe271a00bfbc6a
likhi <likhi@Srinu>
2025-03-29 23:01:28 +0530

    commit

---
+++ SVKP_college/src/org/tnsif/capgemini/c2tc/interfacedemo/FuctionalInterface1.java
@@ -0,0 +1,9 @@
+package org.tnsif.capgemini.c2tc.interfacedemo;
+
+import java.lang.FunctonalInterface;
+@FunctionalInterface
+
+public interface FuctionalInterface1 {
+
+	void sayHello(String name);
+}
